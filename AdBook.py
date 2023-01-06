51d173c58993262f8b08791794e851282efd5ea0
Bartosz Wilgucki <bartosz.wilgucki@gmail.com>
2023-01-07 00:49:17 +0100

    Wizytówki

--- AdBook.py
+++ AdBook.py
@@ -1,6 +1,7 @@
 'Skonstruuj pętlę, która wyświetli całą zawartość listy wizytówek tak'
 ' aby w jednej linii widoczne było imię, nazwisko i adres e-mail właściciela wizytówki.'
 
+import logging
 class Business_card:
     def __init__(self, name, surname, company_name, position, email):
         self.name = name
@@ -9,7 +10,7 @@ class Business_card:
         self.position = position
         self.email = email
 
-BC_1 = Business_card(name = 'Tytus', surname = 'Bomba',  company_name = 'Gwiezdna Flota',position = 'Kapitan', email = 'KapitanBomba@gmail.com')
+BC_1 = Business_card('Tytus','Bomba',  company_name = 'Gwiezdna Flota',position = 'Kapitan', email = 'KapitanBomba@gmail.com')
 
 BC_2 = Business_card(name = 'Janusz', surname = 'Sram', company_name = 'Gwiezdna Flota', position = 'Szeregowy', email = 'JanuszSram@gmail.com')
 
@@ -19,7 +20,14 @@ BC_4 = Business_card(name = 'Michał', surname = 'Jachaś', company_name = 'Gala
 
 BC_5 = Business_card(name = 'Michał', surname = 'Głuś', company_name = 'Kosmici niezrzeszeni', position = 'Szpieg', email = 'Mikele@onet.pl')
 
-list = (BC_1, BC_2, BC_3, BC_4, BC_5)
+BC_list = (BC_1, BC_2, BC_3, BC_4, BC_5)
 
-for Business_card in list:
+for Business_card in BC_list:
     print(Business_card.name,",", Business_card.surname,",", Business_card.email)
+def __str__(self):
+    return f'{self.name} {self.surname} {self.email}'
+
+by_name = sorted(BC_list, key=lambda BC_list: BC_list.name)
+print(by_name)
+#by_surname =
+#by_email =
